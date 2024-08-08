0995f5b5244397ec519b4c7b9f07913b10dc960d
Chanyanuch <chanyanuch_panyadee@cmu.ac.th>
2024-08-08 22:44:41 +0700

    Lab05-4.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
-import type { Config } from 'tailwindcss'
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'sp': '0 3px 12px 0 rgba(0,0,0, 0.2)'
+      }
+    },
   },
   plugins: [],
-} satisfies Config
+}
